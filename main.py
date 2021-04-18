1a3e4fda5c8ac3bec70c7b69cabd08e9e47ecaf3
Berugatoi <romandadakov@yandex.ru>
2021-04-18 15:29:51 +0300

    Плохой дизайн и его отображение.

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 from data.db_session import create_session, global_init
 from init_functions import *
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/signup')
+def singup():
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/viewprofile')
+def viewprofile():
+    return render_template('view_profile.html')
+
+
+@app.route('/editprofile')
+def editprofile():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     global_init('store.db')

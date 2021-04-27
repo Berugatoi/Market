6092aada52d640696adedb60874ef47a88f1d247
Zelimkhan4 <vip.guchiev@mail.ru>
2021-04-27 20:00:35 +0300

    Добавил корзину

--- static/main.js
+++ static/main.js
@@ -9,12 +9,12 @@ let Switch = function (prod_id) {
     if (current.src == path + 'static/icons/empty_heart.jpg') {
         current.src = 'static/icons/full_heart.jpg';
         let cookies = convertStringToObject(document.cookie);
-            if (cookies[' UserCookie']){
+            if (cookies['UserCookie']){
                 console.log(String(prod_id));
                 console.log(cookies[' UserCookie'].split(':'));
-                console.log((String(prod_id) in cookies[' UserCookie'].split(':')));
-                if (!cookies[' UserCookie'].includes(String(prod_id))) {
-                    let new_val = 'UserCookie=' + cookies[' UserCookie'] + ':' + String(prod_id);
+                console.log((String(prod_id) in cookies['UserCookie'].split(':')));
+                if (!cookies['UserCookie'].includes(String(prod_id))) {
+                    let new_val = 'UserCookie=' + cookies['UserCookie'] + ':' + String(prod_id);
                     document.cookie = new_val;
              }
 
@@ -31,11 +31,25 @@ let Switch = function (prod_id) {
 }
 
 
+let addToCart = function(prod_id){
+    let cookies = convertStringToObject(document.cookie);
+    if (cookies['Cart']){
+        if (!cookies['Cart'].includes(String(prod_id))) {
+            let new_val = 'Cart=' + cookies['Cart'] + ':' + String(prod_id);
+            document.cookie = new_val;
+     }
+}
+    else {
+        document.cookie = 'Cart=' + String(prod_id)
+    }
+}
+
+
 var convertStringToObject = function(s){
     let res = {},
     tmp = '';
 
-    var a = s.split(';');
+    var a = s.split('; ');
     a.forEach(function(item, i, arr){
         let v = item.split('=');
         res[v[0]] = v[1];

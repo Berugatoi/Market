555630e467ed0ee6dc4bfc505c1c04e0f39ca3f0
Zelimkhan4 <vip.guchiev@mail.ru>
2021-04-29 20:32:40 +0300

    Восстановил картинки

--- functions/format_image.py
+++ functions/format_image.py
@@ -1,9 +1,13 @@
 from PIL import Image
 import os
+from uuid import uuid5, NAMESPACE_OID
 
 
 def resize_image(image_src):
-    im = Image.open(image_src)
-    size = (200, 200)
-    im = im.resize(size)
-    im.save(image_src)
+    try:
+        im = Image.open(image_src)
+        size = (200, 200)
+        im = im.resize(size)
+        im.save(image_src)
+    except:
+        pass
